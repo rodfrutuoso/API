9717381c6108cb14ae1e9046d1ec2d8e69dbc0ba
rodfrutuoso <rodfrutuoso@hotmail.com>
2023-10-12 21:28:17 -0300

    att

---
+++ README.md
@@ -0,0 +1,9 @@
+# Anotações
+
+no power shell: npm init -y //cria uma aplicação node
+já cria o package.json
+
+pra instalar o express: npm install express --save
+o package-lock.json tem as dependências do projeto. Dá pra instalar td só com: npm install
+
+adicionei essa linha em scripts no packagejson pra inicializar a aplicação: "start": "node ./src/server.js",
--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
-//const que invoca a biblioteca
+//configurações do server
+//const que invoca a biblioteca express
 const express = require("express");
 
+//inicializa o express pra poder usar
 const app = express();
 
+
 app.get("/:id/:user", (request, response) =>{
+    //parametros, Route Params que não são obrigatórios
     const {id, user} = request.params
 
     response.send(`ooooi, migo ${id} para o usuário: ${user}`)
 })
 
-
+//criando uma requisição do tipo get, pra ficar no local host somente troque o /users por /
 app.get("/users", (request, response)=>{
+    //cria um query params, nesse caso eu tenho q passar localhost:3333/?page=2&limit=4
     const {page, limit} = request.query;
 
     response.send(`A pagina é ${page*2} e o limite é ${limit}`)
@@ -18,6 +23,8 @@ app.get("/users", (request, response)=>{
 
 app.post()
 
+//a porta do node
 const PORT = 3333;
 
-app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`));
+//eh o que ele faz quando inicia
+app.listen(PORT,()=> console.log(`Server está rodnado na porta ${PORT}`));
