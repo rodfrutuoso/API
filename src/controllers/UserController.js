0ce956d160e818359f0f75aada45b9b9ad0b084f
rodfrutuoso <rodfrutuoso@hotmail.com>
2023-10-15 21:56:44 -0300

    adicionando controller

---
+++ src/controllers/UserController.js
@@ -0,0 +1,12 @@
+class UserController {
+    /**
+     * index - GET para listar vários registros
+     * show - GET para exibir um registro específico
+     * create - POST para criar um registro
+     * upadte - PUT para atualizar o registro
+     * delete - DELETE para remover um registro
+     */
+
+}
+
+module.exports = UserController
