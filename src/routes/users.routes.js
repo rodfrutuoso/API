905e48cc2438c40f59080115d17df24f23593ac1
rodfrutuoso <rodfrutuoso@hotmail.com>
2023-10-19 21:56:33 -0300

    F323 aprendendo middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -10,8 +10,12 @@ const userController = new UserController()
 //inicializa as rotas
 const userRoutes = Router()
 
+//coloco o middleware para todas as rotas com .use
+userRoutes.use(myMiddleware)
+
 //método post
 userRoutes.post("/",userController.create)
+//userRoutes.post("/", myMiddleware,userController.create)
 
 // userRoutes.post("/", (request, response) => {
 //     //const { name, email, password } = request.body;
@@ -20,6 +24,16 @@ userRoutes.post("/",userController.create)
 //     //response.json({ name, email, password })
 
 // })
+
+
+function myMiddleware(request,response,next){
+    console.log("pasosu pelo middleware")
+    if(!request.body.isAdmin){
+        return response.status(401).json({message: "O usuário não é Administrador"})
+    }
+
+    next()
+}
 
 //exportando o arquivo pra outras pessoas poderem usar
 module.exports = userRoutes;
